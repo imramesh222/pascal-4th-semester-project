d20eb49c509168ca56f5069dbaadaa6b3a47c09c
imramesh222 <imrameshrawat@gmail.com>
2024-04-08 18:15:30 +0545

    first comit

---
+++ about.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fashion Store</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+  <link rel="stylesheet" href="about.css">
+</head>
+<body>
+  <section id="header">
+    <a href="home_page.php"><img src="img/logo1.png" class="logo" alt=""></a>
+    <div>
+    <ul id="navbar">
+      <li><a href="user-profile.php">Home</a></li>
+      <li><a href="shop.php">Shop</a></li>
+      <li><a class="active" href="about.php">About</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li id="lg-bag"><a href="cart.php"><i class="fas fa-shopping-cart"></i></a></li>
+      <a href="#" id="close"><i class="fa-solid fa-xmark"></i></a>
+    </ul>
+    </div>
+    <div class="mobile">
+      <a href="cart.php"><i class="fas fa-shopping-cart"></i></a>
+      <i id="bar" class="fas fa-outdent"></i>
+    </div>
+  </section>
+  <section id="contact">
+    <h1>ABOUT US</h1>
+    <p>
+    Our company was founded in 2008. We operate three warehouses and a showroom of 3000m2 only a few kilometers away from Budapest, very convenient to access both by plane or car. 
+    
+    We sell leftover stocks, surplus stocks, sample collections of well-known fashion brands. All products are genuine, A-grade and brand new with their original tags on. We buy our garments directly from European distributors and always make sure that the quality of the products is impeccable. 
+    
+    Our range of products is growing wider continuously, we offer more than 60 well-known and popular brands to our partners. Our stock is renewed constantly, with daily new offers (you can subscribe to our newsletter). Our stocklots are predominantly made up of leftover or sample collections of popular and globally known fashion brands, however, at the same time we include more exclusive, less known, but excellent quality brands too.
+    
+    
+    We value flexibility, try to offer reasonable and affordable prices, and are aware well of the trends and changes on the market. Unlike other companies, we offer the unique possibility to select goods piece by piece, regarding models and sizes. In our showroom there are more than 40.000 articles on display. At the same time, our products can be hand-picked from our warehouse, from more than 1.000.000 articles in original boxes or sacks. We also offer smaller mixes of clothes to our clients. Sample mixes of minimum quantity, as well as sales on pallets are also possible, depending on our clients’ demands. We ship worldwide with DPD.
+    
+    
+    
+    Wholesalers and retailers both have the possibility to purchase from us, the minimum purchase quantity is 30 pieces. Thanks to our long-term, personal and international business relationships and connections, we are offering stocklots at the most advantageous prices.
+    Our team speaks Hungarian, Romanian, English, French, Arabic and Danish. We encourage you to come and visit us for a coffee, so you can see our showroom and current offers in person. We will help you arrange transportation, accommodation, should you decide to visit us. If needed, business advice and guidance is also offered by our experienced sales team, if you are new in the fashion industry.
+    
+    We hope to see you soon!</p>
+  
+  </section>
+  <footer class="section-p1">
+    <div class="col">
+      <img class="logo" src="img/logo.png" alt="">
+      <h4>Contact</h4>
+      <p><strong>Address: </strong> Satdobato,Lalitpur</p>
+      <p><strong>Phone: </strong> +9779868043277</p>
+      <p><strong>Hours: </strong> 10:00 - 18:00, Sun - Fri</p>
+      <div class="follow">
+        <h4>Follow us</h4>
+        <div class="icon">
+          <i class="fab fa-facebook-f"></i>
+          <i class="fab fa-twitter"></i>
+          <i class="fab fa-instagram"></i>
+          <i class="fab fa-pinterest-p"></i>
+          <i class="fab fa-youtube"></i>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <h4>About</h4>
+      <a href="#">About us</a>
+      <a href="#">Delivery Information</a>
+      <a href="#">Privacy Policy</a>
+      <a href="#">Terms & Conditions</a>
+      <a href="#">Contact us</a>
+    </div>
+    <div class="col">
+      <h4>My Account</h4>
+      <a href="#">Sign In</a>
+      <a href="#">View Cart</a>
+      <a href="#">My Wishlist</a>
+      <a href="#">Help</a>
+    </div>
+
+    
+  </footer>
+
+</body>
